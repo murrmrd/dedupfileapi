df5c8b928bc60c7d5a96707f07b52fbaff0c16a1
olivierolejniczak <olivierolejniczak@5baee6d4-cf85-11de-9ec6-abeeca6af3b2>
2009-11-25 13:51:01 +0000

    continue updating batch test

--- trunk/DedupFileAPI/src/DedupFileAPI_main.py
+++ trunk/DedupFileAPI/src/DedupFileAPI_main.py
@@ -248,6 +248,43 @@ def ProcessFile(db,filename):
         os.remove(newfile)
     os.rename(filename,newfile)
 
+def dirwalk(dir):
+    try:
+       for f in os.listdir(dir):
+        fullpath = os.path.join(dir,f)
+        if os.path.isdir(fullpath) and not os.path.islink(fullpath):
+            if not len(os.listdir(fullpath)):
+                print "1",  fullpath + os.sep
+            else:
+                for x in dirwalk(fullpath):  # recurse into subdir
+                    if os.path.isdir(x):
+                        if giveDirs:
+                            print "2",  x
+                    else:
+                        print "3",  x
+        else:
+            print "4", fullpath
+
+    finally:
+        print "6"
+
+def testfile(db,dr,flst):
+    for f in  flst:
+        fullf = os.path.join(dr,f)
+        if os.path.isfile(fullf):
+                print "** Dedup file: ",fullf
+
+                stats = os.stat(fullf)
+                filesize =  stats[stat.ST_SIZE]
+#                totalfilesize = totalfilesize + filesize
+
+                ProcessFile(db,fullf)
+
+
+def dtstat(dtroot,db):
+    os.path.walk(dtroot,testfile,db)
+
+
 
 def BatchTest(StartDir, Nbfiles,NbDir, MaxFileSize):
     ""
@@ -262,21 +299,11 @@ def BatchTest(StartDir, Nbfiles,NbDir, MaxFileSize):
     db = DedupDB.DedupDB()
 
     print "Start archiving at ",startDir
+    #dirwalk(startDir)
+    os.path.walk(startDir,testfile,db)
+    
 
-    directories = [startDir]
-    while len(directories)>0:
-        directory = directories.pop()
-        print "* Process directory: ",directory
-        for name in os.listdir(directory):
-            fullpath = os.path.join(directory,name)
-            if os.path.isfile(fullpath):
-                print "** Dedup file: ",fullpath
 
-                stats = os.stat(fullpath)
-                filesize =  stats[stat.ST_SIZE]
-                totalfilesize = totalfilesize + filesize
-
-                ProcessFile(db,fullpath)
     
 #    TreeID = db.getfiletreeidbypath(startDir)
 #    NodeIDList = db.listfiletree(TreeID,False)
@@ -295,7 +322,7 @@ def BatchTest(StartDir, Nbfiles,NbDir, MaxFileSize):
     databasesize = stats[stat.ST_SIZE]
     print "### TotalFileSize= ",totalfilesize
     print "### TotalDatabaseSize= ", databasesize
-    print "### TotalFileSize= ", float(100*databasesize/totalfilesize)
+#    print "### TotalFileSize= ", float(100*databasesize/totalfilesize)
 
 
 
@@ -310,7 +337,7 @@ if __name__ == "__main__":
     Profilerfile2 = os.path.join(os.getenv('HOME'),"DedupFileAPI.cachegrind")
     prof = hotshot.Profile(Profilerfile1)
     prof.start()
-    BatchTest(startDir,2, 2, 1024)
+    BatchTest(startDir,5,4 , 1024)
     prof.stop()
     prof.close()
     #subprocess.call("hotshot2calltree","-o "+ Profilerfile2+ " "+Profilerfile1 )
